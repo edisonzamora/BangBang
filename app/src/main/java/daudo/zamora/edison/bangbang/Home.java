1ba629d68674c63eab309f5ceb2d212e66dad1d3
edisonzamora <edison.zamora.daudo@gmail.com>
2018-03-12 22:04:23 +0100

    cambio de prueba
    un cambio para probar

--- app/src/main/java/daudo/zamora/edison/bangbang/Home.java
+++ app/src/main/java/daudo/zamora/edison/bangbang/Home.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,7 @@ public class Home extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Toast.makeText(getApplicationContext(),"pruba",Toast.LENGTH_LONG).show();
     }
 
     @Override
